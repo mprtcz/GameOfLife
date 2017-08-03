99fac445a7b0e003271d789352429f4d1e4395b4
mprtcz <azet89@gmail.com>
2017-08-03 18:57:40 +0200

    First commit with main files;
    The Project has necessary files now, pom, gitignore, as well as files needed to load the window and a logger;

---
+++ src/main/java/com/mprtcz/gameoflife/App.java
@@ -0,0 +1,30 @@
+package com.mprtcz.gameoflife;
+
+import com.mprtcz.gameoflife.logger.AppLogger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Michal_Partacz
+ * @since 03.08.17
+ */
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainMenu.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("Battleships");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        AppLogger.initializeLogger();
+        launch(args);
+    }
+}
